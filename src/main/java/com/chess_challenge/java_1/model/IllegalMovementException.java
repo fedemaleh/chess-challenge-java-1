372c7466c2299b5a544978afb5d62e98f41fd53b
Federico Maleh <federico.maleh@wildlifestudios.com>
2022-01-11 00:46:12 -0300

    Add Piece interface

---
+++ src/main/java/com/chess_challenge/java_1/model/IllegalMovementException.java
@@ -0,0 +1,7 @@
+package com.chess_challenge.java_1.model;
+
+public class IllegalMovementException extends Exception {
+    public IllegalMovementException(Piece piece, Square square) {
+        super(String.format("Cannot move a %s %s from %s to %s", piece.color(), piece.type(), piece.position().toString(), square.toString()));
+    }
+}
---
+++ src/main/java/com/chess_challenge/java_1/model/Piece.java
@@ -0,0 +1,52 @@
+package com.chess_challenge.java_1.model;
+
+import java.util.List;
+
+/**
+ * Represents a chess piece. It contains all the methods needed to model the movement of the pieces.
+ */
+public interface Piece {
+    /**
+     * Gets the movements a piece can make.
+     *
+     * @return a list of squares where the piece can move.
+     */
+    List<Square> moves();
+
+    /**
+     * Gets the attack targets a piece has.
+     *
+     * @return a list of squares that are threatened by the piece.
+     */
+    List<Square> attacks();
+
+    /**
+     * Gets the current position of the piece.
+     *
+     * @return the square of the piece.
+     */
+    Square position();
+
+    /**
+     * Moves a piece to a different square.
+     *
+     * @param square the target square for the piece.
+     * @return the piece in the new square.
+     * @throws IllegalMovementException when the piece can't move to the desired square.
+     */
+    Piece moveTo(Square square) throws IllegalMovementException;
+
+    /**
+     * Gets the color of the piece.
+     *
+     * @return the color of the piece.
+     */
+    Color color();
+
+    /**
+     * Gets the type of the piece.
+     *
+     * @return the type of the piece.
+     */
+    Type type();
+}
