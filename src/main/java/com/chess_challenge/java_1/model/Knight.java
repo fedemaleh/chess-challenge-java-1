6184daecadacfa1073948fd6917ad090043fdd22
Federico Maleh <federico.maleh@wildlifestudios.com>
2022-01-24 23:05:11 -0300

    Knight moves

---
+++ src/main/java/com/chess_challenge/java_1/model/Knight.java
@@ -0,0 +1,79 @@
+package com.chess_challenge.java_1.model;
+
+import com.google.common.collect.Maps;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Knight implements Piece {
+    private final Color color;
+    private final Square square;
+
+    public Knight(Color color, Square square) {
+        this.color = color;
+        this.square = square;
+    }
+
+    @Override
+    public List<Square> moves() {
+        List<Square> moves = new ArrayList<>();
+
+        moves.addAll(this.verticalMoves());
+
+        moves.addAll(this.horizontalMoves());
+
+        return moves;
+    }
+
+    private List<Square> verticalMoves() {
+        return IntStream.of(this.position().getRow() - 2, this.position().getRow() + 2)
+                .filter(row -> row >= 1 && row <= 8)
+                .boxed()
+                .flatMap(row ->
+                        IntStream.of(this.position().getColumn() - 1, this.position().getColumn() + 1)
+                                .filter(col -> col >= 'a' && col <= 'h')
+                                .mapToObj(col -> new Square((char) col, row))
+                )
+                .collect(Collectors.toList());
+    }
+
+    private List<Square> horizontalMoves() {
+        return IntStream.of(this.position().getColumn() - 2, this.position().getColumn() + 2)
+                .filter(col -> col >= 'a' && col <= 'h')
+                .boxed()
+                .flatMap(col ->
+                        IntStream.of(this.position().getRow() - 1, this.position().getRow() + 1)
+                                .filter(row -> row >= 1 && row <= 8)
+                                .mapToObj(row -> new Square((char) (int) col, row))
+                )
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Square> attacks() {
+        return null;
+    }
+
+    @Override
+    public Square position() {
+        return this.square;
+    }
+
+    @Override
+    public Knight moveTo(Square square) throws IllegalMovementException {
+        return null;
+    }
+
+    @Override
+    public Color color() {
+        return this.color;
+    }
+
+    @Override
+    public Type type() {
+        return Type.KNIGHT;
+    }
+}
---
+++ src/test/java/com/chess_challenge/java_1/model/KnightMovementTest.java
@@ -0,0 +1,174 @@
+package com.chess_challenge.java_1.model;
+
+import com.google.common.collect.Lists;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KnightMovementTest {
+    @Test
+    @Timeout(value = 5)
+    void knight_in_d4_has_8_moves() throws IllegalSquareException {
+        // Given a white Knight in square D4.
+        Square d4 = new Square('d', 4);
+
+        Knight knight = new Knight(Color.WHITE, d4);
+
+        // When the knight is asked for his moves
+        List<Square> moves = knight.moves();
+
+        List<Square> expectedMoves = Lists.newArrayList(
+                // Forward moves
+                new Square('c', 6), new Square('e', 6),
+                // Backward moves
+                new Square('c', 2), new Square('e', 2),
+                // Leftward moves
+                new Square('b', 3), new Square('b', 5),
+                // Rightward moves
+                new Square('f', 3), new Square('f', 5)
+        );
+
+        // all the expected moves where found
+        assertAll(
+                expectedMoves.stream().map((square) -> () -> assertTrue(moves.contains(square), () -> String.format("Move: %s was not found", square)))
+        );
+
+        // no additional expected move found
+        assertAll(
+                moves.stream().map((square) -> () -> assertTrue(expectedMoves.contains(square), () -> String.format("Move: %s is not expected", square)))
+        );
+    }
+
+    @Test
+    @Timeout(value = 5)
+    void knight_in_a1_has_2_moves() throws IllegalSquareException {
+        // Given a white Knight in square A1.
+        Square a1 = new Square('a', 1);
+
+        Knight knight = new Knight(Color.WHITE, a1);
+
+        // When the knight is asked for his moves
+        List<Square> moves = knight.moves();
+
+        List<Square> expectedMoves = Lists.newArrayList(
+                // Forward moves
+                new Square('b', 3),
+                // Backward moves
+                // Leftward moves
+                // Rightward moves
+                new Square('c', 2)
+        );
+
+        // all the expected moves where found
+        assertAll(
+                expectedMoves.stream().map((square) -> () -> assertTrue(moves.contains(square), () -> String.format("Move: %s was not found", square)))
+        );
+
+        // no additional expected move found
+        assertAll(
+                moves.stream().map((square) -> () -> assertTrue(expectedMoves.contains(square), () -> String.format("Move: %s is not expected", square)))
+        );
+    }
+
+    @Test
+    @Timeout(value = 5)
+    void knight_in_h1_has_2_moves() throws IllegalSquareException {
+        // Given a white Knight in square H1.
+        Square h1 = new Square('h', 1);
+
+        Knight knight = new Knight(Color.WHITE, h1);
+
+        // When the knight is asked for his moves
+        List<Square> moves = knight.moves();
+
+        List<Square> expectedMoves = Lists.newArrayList(
+                // Forward moves
+                new Square('g', 3),
+                // Backward moves
+                // Leftward moves
+                new Square('f', 2)
+                // Rightward moves
+        );
+
+        // all the expected moves where found
+        assertAll(
+                expectedMoves.stream().map((square) -> () -> assertTrue(moves.contains(square), () -> String.format("Move: %s was not found", square)))
+        );
+
+        // no additional expected move found
+        assertAll(
+                moves.stream().map((square) -> () -> assertTrue(expectedMoves.contains(square), () -> String.format("Move: %s is not expected", square)))
+        );
+    }
+
+    @Test
+    @Timeout(value = 5)
+    void knight_in_a8_has_2_moves() throws IllegalSquareException {
+        // Given a white Knight in square A8.
+        Square a8 = new Square('a', 8);
+
+        Knight knight = new Knight(Color.WHITE, a8);
+
+        // When the knight is asked for his moves
+        List<Square> moves = knight.moves();
+
+        List<Square> expectedMoves = Lists.newArrayList(
+                // Forward moves
+                // Backward moves
+                new Square('b', 6),
+                // Leftward moves
+                // Rightward moves
+                new Square('c', 7)
+        );
+
+        // all the expected moves where found
+        assertAll(
+                expectedMoves.stream().map((square) -> () -> assertTrue(moves.contains(square), () -> String.format("Move: %s was not found", square)))
+        );
+
+        // no additional expected move found
+        assertAll(
+                moves.stream().map((square) -> () -> assertTrue(expectedMoves.contains(square), () -> String.format("Move: %s is not expected", square)))
+        );
+    }
+
+    @Test
+    @Timeout(value = 5)
+    void knight_in_h8_has_2_moves() throws IllegalSquareException {
+        // Given a white Knight in square H8.
+        Square d8 = new Square('h', 8);
+
+        Knight knight = new Knight(Color.WHITE, d8);
+
+        // When the knight is asked for his moves
+        List<Square> moves = knight.moves();
+
+        List<Square> expectedMoves = Lists.newArrayList(
+                // Forward moves
+                // Backward moves
+                new Square('g', 6),
+                // Leftward moves
+                new Square('f', 7)
+                // Rightward moves
+        );
+
+        // all the expected moves where found
+        assertAll(
+                expectedMoves.stream().map((square) -> () -> assertTrue(moves.contains(square), () -> String.format("Move: %s was not found", square)))
+        );
+
+        // no additional expected move found
+        assertAll(
+                moves.stream().map((square) -> () -> assertTrue(expectedMoves.contains(square), () -> String.format("Move: %s is not expected", square)))
+        );
+    }
+}
