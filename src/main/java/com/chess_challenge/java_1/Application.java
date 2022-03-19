36ef8b3d6dacb3db0123681663c46962a5a07eb9
Federico Maleh <federico.maleh@wildlifestudios.com>
2022-03-19 04:16:58 -0300

    Add configuration file to add flags

--- src/main/java/com/chess_challenge/java_1/Application.java
+++ src/main/java/com/chess_challenge/java_1/Application.java
@@ -1,12 +1,18 @@
 package com.chess_challenge.java_1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        logger.info("Stats Repository Strategy: " + context.getEnvironment().getProperty("stats.repository"));
     }
 }
