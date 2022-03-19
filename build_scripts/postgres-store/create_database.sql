887af464600e9aae028ded69f630ec63e1eec88d
Federico Maleh <federico.maleh@wildlifestudios.com>
2022-03-19 05:42:22 -0300

    Improve Hibernate Schema

--- build_scripts/postgres-store/create_database.sql
+++ build_scripts/postgres-store/create_database.sql
@@ -1,20 +1,12 @@
 DROP DATABASE IF EXISTS CHESS_CHALLENGE;
 CREATE DATABASE CHESS_CHALLENGE;
 
-CREATE TABLE IF NOT EXISTS WINNERS (
-    white INTEGER,
-    black INTEGER,
-    no_winner INTEGER
+CREATE TABLE IF NOT EXISTS RESULTS (
+    id SERIAL CONSTRAINT results_pk PRIMARY KEY,
+    winner CHAR(9) NOT NULL CHECK (winner IN ('white', 'black', 'no_winner')),
+    piece CHAR(6) NOT NULL CHECK (piece IN ('bishop', 'king', 'knight', 'pawn', 'queen', 'rook', 'none'))
 );
 
-CREATE TABLE IF NOT EXISTS PIECES (
-    bishop INTEGER,
-    king INTEGER,
-    knight INTEGER,
-    pawn INTEGER,
-    queen INTEGER,
-    rook INTEGER
-);
-
--- INSERT INTO WINNERS VALUES (0,0,0);
--- INSERT INTO PIECES VALUES (0,0,0,0,0,0);
+INSERT INTO RESULTS (winner, piece) VALUES ('no_winner', 'none');
+INSERT INTO RESULTS (winner, piece) VALUES ('white', 'bishop');
+INSERT INTO RESULTS (winner, piece) VALUES ('black', 'queen');
